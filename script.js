42224ca345f04bdfff08f2628b4a1d2a5b10984a
Ceekaye Lipnica <ceekayelipnica04@gmail.com>
2023-12-04 15:59:00 +0800

    complete

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ toggleMenu.addEventListener("click", () => {
     toggleMenu.classList.toggle("open");
     headNav.classList.toggle("open");
 });
+
+// CHANGING THE COLOR OF HEADER 
+const headerr = document.querySelector(".header-desktop");
+window.addEventListener("scroll", () => { 
+	if (window.pageYOffset >= 100) {
+		headerr.classList.add("changeToBlack");
+	} else {
+		headerr.classList.remove("changeToBlack");
+	}
+});
